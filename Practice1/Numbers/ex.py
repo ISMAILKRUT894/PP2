83c1a42afdbb67a2808a19abb604d125013a66c5
ISMAILKRUT894 <unyisa11@gmail.com>
2026-02-03 15:36:31 +0500

    Practice2

---
+++ Practice1/Numbers/ex.py
@@ -0,0 +1,8 @@
+in1 = int(input())
+in2 = int(input())
+if in1>in2: 
+    print(in1)
+elif in2 >in1 :
+    print(in1)
+else:
+    print("equal")
---
+++ Practice2/IfElse/HandIf.py
@@ -0,0 +1,7 @@
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
