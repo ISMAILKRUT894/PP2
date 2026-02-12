5e3aa7cc53d255656fe947a33f00cd88b2a1ece9
ISMAILKRUT894 <unyisa11@gmail.com>
2026-02-12 14:28:19 +0500

    Practice2

--- Practice2/ForLoop.py/ForLoopContinue.py
+++ Practice2/ForLoop.py/ForLoopContinue.py
@@ -1,5 +1,6 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    continue
-  print(x)
+a = [12, 34, 11, 76, 98]
+max = a[0]
+for i in range(1, len(a)):
+    if a[i] < max:
+        max = a[i]
+print(max)
